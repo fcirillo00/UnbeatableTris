27e3b6f3a25f28623359ab813f15b74624f5bb74
Francesco Cirillo <93737876+fcirillo00@users.noreply.github.com>
2022-09-03 19:44:51 +0200

    Add files via upload

--- tris.py
+++ tris.py
@@ -1,5 +1,6 @@
 from copy import copy
 import random
+from time import time
 
 class Tris:
     def __init__(self, turn = 'x'):
@@ -8,11 +9,23 @@ class Tris:
                       '_', '_', '_']
         self.turn = turn
 
-    def copyBoard(self):
-        board2 = Tris()
-        board2.board = copy(self.board)
-        board2.turn = self.turn
-        return board2
+    def showBoard(self):
+        board = copy(self.board)
+        for i in range(9):
+            if board[i] == "_":
+                board[i] = " "
+        print(board[0] + " | " + board[1] + " | " + board[2])
+        print("__|___|__")
+        print(board[3] + " | " + board[4] + " | " + board[5])
+        print("__|___|__")
+        print(board[6] + " | " + board[7] + " | " + board[8])
+        print("  |   |  ")
+
+    def getCell(self, row, column):
+        return self.board[row*3 + column]
+
+    def setCell(self, row, column):
+        self.board[row*3 + column] = self.turn
 
     def makeMove(self, row, column):
         # rows start at 1
@@ -34,24 +47,6 @@ class Tris:
         self.board = board
         self.turn = 'o' if self.turn == 'x' else 'x'
 
-    def showBoard(self):
-        board = copy(self.board)
-        for i in range(9):
-            if board[i] == "_":
-                board[i] = " "
-        print(board[0] + " | " + board[1] + " | " + board[2])
-        print("__|___|__")
-        print(board[3] + " | " + board[4] + " | " + board[5])
-        print("__|___|__")
-        print(board[6] + " | " + board[7] + " | " + board[8])
-        print("  |   |  ")
-    
-    def getCell(self, row, column):
-        return self.board[row*3 + column]
-
-    def setCell(self, row, column):
-        self.board[row*3 + column] = self.turn
-
     def getRows(self):
         rows = []
         for i in range(3):
@@ -71,21 +66,8 @@ class Tris:
         second_diag = [self.getCell(x, 2-x) for x in range(3)]
         return [first_diag, second_diag]
 
-    def getNextPossibleMoves(self):
-        indices = [i for i, cell in enumerate(self.board) if cell == "_"]
-        board = self.copyBoard()
-        children = []
-        for i in indices:
-            board2 = board.copyBoard()
-            board2.board[i] = self.turn
-            board2.turn = 'o' if self.turn == 'x' else 'x'
-            children.append(board2.copyBoard())
-        return children
-
-    def countEmptyCells(self):
-        return self.board.count('_')
-
-    def utility(self):
+    def result(self):
+        # returns 1 if it's a win, 0 if draw/unfinished, -1 if it's a loss
         win = ['x', 'x', 'x']
         lose = ['o', 'o', 'o']
 
@@ -94,17 +76,50 @@ class Tris:
         diagonals = self.getDiagonals()
         all = rows + columns + diagonals
         
-        # utility value changes depending on how many rounds are left
-        # a win with 4 empty cells is worth more than a win with no cells left
         if win in all:
-            return 1 + self.countEmptyCells()
+            return 1
         if lose in all:
-            return -(1 + self.countEmptyCells())
-        # draw
-        return 0    
+            return -1
+        # draw/unfinished
+        return 0  
+
+    def countEmptyCells(self):
+        return self.board.count('_')
 
     def isGameOver(self):
-        return self.countEmptyCells() == 0 or self.utility() != 0
+        return self.countEmptyCells() == 0 or self.result() != 0
+
+    def createCopy(self):
+        trisCopy = Tris()
+        trisCopy.board = copy(self.board)
+        trisCopy.turn = self.turn
+        return trisCopy
+
+    def getNextPossibleMoves(self):
+        indices = [i for i, cell in enumerate(self.board) if cell == "_"]
+        father = self.createCopy()
+        children = []
+        for i in indices:
+            child = father.createCopy()
+            child.board[i] = self.turn
+            child.turn = 'o' if self.turn == 'x' else 'x'
+            children.append(child.createCopy())
+        return children
+
+    def utility(self):    
+        # utility value changes depending on how many rounds are left
+        # a win with 4 empty cells is worth more than a win with no cells left
+        
+        # if win
+        if self.result() == 1:
+            return 1 + self.countEmptyCells()
+        # if loss
+        if self.result() == -1:
+            return - (1 + self.countEmptyCells())
+        # if draw
+        return 0
+
+
 
 # minimax with no pruning, much slower at first
 def minimax(node: Tris):
@@ -114,24 +129,26 @@ def minimax(node: Tris):
         return node.utility(), node
     
     if node.turn == 'x':
+        # player MAX
         max_utility = -1000
-        best_option = None
+        best_node = None
         for child in children:
-            utility, game_state = minimax(child)
+            utility, current_node = minimax(child)
             if utility > max_utility:
                 max_utility = utility 
-                best_option = child
-        return max_utility, best_option
+                best_node = child
+        return max_utility, best_node
 
     if node.turn == 'o':
+        # player MIN
         min_utility = 1000
-        best_option = None
+        best_node = None
         for child in children:
-            utility, game_state = minimax(child)
+            utility, current_node = minimax(child)
             if utility < min_utility:
                 min_utility = utility 
-                best_option = child
-        return min_utility, best_option
+                best_node = child
+        return min_utility, best_node
 
 # minimax with pruning, almost instantaneous
 def minimaxPruning (node: Tris, alpha = -1000, beta = 1000):
@@ -142,40 +159,41 @@ def minimaxPruning (node: Tris, alpha = -1000, beta = 1000):
     if node.turn == 'x':
         # alpha is the best explored option for player X (max)
         max_utility = -1000
-        best_option = None
+        best_node = None
         for child in children:
-            utility, game_state = minimaxPruning(child, alpha, beta)
+            utility, current_node = minimaxPruning(child, alpha, beta)
             if utility > max_utility: 
                 max_utility = utility
-                best_option = child
+                best_node = child
 
             alpha = max(max_utility, alpha)
             if alpha >= beta:
                 break
                 
-        return max_utility, best_option
+        return max_utility, best_node
     
     if node.turn == 'o':
         # beta is the best explored option for player O (min)
         min_utility = 1000
-        best_option = None
+        best_node = None
         for child in children:
             utility, game_state = minimaxPruning(child, alpha, beta)
             if utility < min_utility: 
                 min_utility = utility
-                best_option = child
+                best_node = child
 
             beta = min(min_utility, beta)
             if alpha >= beta:
                 break
                 
-        return min_utility, best_option
+        return min_utility, best_node
             
 
 def main():
     inp = 'a'
     while inp.lower() != 'q':
         first = str(input("Do you want to play first? (Y o N)\n"))
+        print("You are playing as O.")
         if first.lower() != 'y':
             game = Tris('x')
             # all moves at the first round are of equal utility
@@ -201,9 +219,8 @@ def main():
         else:
             print("It's a draw.")
         inp = input("Send Q to exit, or any key to restart.\n")
-
+    
 if __name__== "__main__":
     random.seed()
     main()
-
 
